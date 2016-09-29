806941ff90990b0759412da5a02a65e42d0f08bd
Derrick <dward330@gmail.com>
2016-09-28 20:15:06 -0400

    Author: Derrick Ward
    Changes:
     -Added Extension Method Example.

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringExtensionMethods;
 
 namespace SandBox
 {
@@ -10,6 +11,27 @@ namespace SandBox
     {
         static void Main(string[] args)
         {
+            Demo_ExtensionMethods(5);
+        }
+
+        /// <summary>
+        /// This Method Demos Extension Methods
+        /// </summary>
+        public static void Demo_ExtensionMethods(int characterLength = 0) {
+            List<String> wordList = new List<string>();
+            wordList.Add("Test");
+            wordList.Add("Tested");
+            wordList.Add("Tests");
+            wordList.Add("Testing");
+
+            wordList.ElementsLargerThan(characterLength).PrintStringList();
+
+            PolitelyEndProgram();
+        }
+
+        public static void PolitelyEndProgram() {
+            Console.WriteLine("\nProgram is finished running. Press Any Key to End Program...");
+            Console.ReadKey(true);
         }
     }
 }
---
+++ SandBox/StringExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionMethods
+{
+    /// <summary>
+    /// This class will contain some of the extension methods for List<String> Type Objects
+    /// </summary>
+    static class StringExtensionMethods
+    {
+        /// <summary>
+        /// Return IEnumerable type containing items that have more characters than @numOfCharacters
+        /// </summary>
+        /// <param name="obj"> Object this method is applied to</param>
+        /// <param name="numOfCharacters"> The number of characters list item needs to be larger than</param>
+        /// <returns></returns>
+        public static IEnumerable<String> ElementsLargerThan(this List<String> obj, int numOfCharacters) {
+            return obj.Where(x => x.Length > numOfCharacters).ToList();
+        }
+
+        /// <summary>
+        /// Prints all the items in IEnumerable to the Console Window
+        /// </summary>
+        /// <param name="obj"> Object this method is applied to</param>
+        public static void PrintStringList(this IEnumerable<String> obj) {
+            foreach (String item in obj)
+                Console.WriteLine(item);
+        }
+    }
+}
